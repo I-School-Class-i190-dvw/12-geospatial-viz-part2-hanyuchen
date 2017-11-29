d83707a45a5b298beb81bcdc7d39804bfc15abe5
Erica <erica@Ericade-MacBook-Air.local>
2017-11-29 11:52:29 -0800

    class12

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 console.log("hello world")
 
+// set the width and weight
 var width = Math.max(960, window.innerWidth),
     height = Math.max(500, window.innerHeight);
 
+// use pi to get tau
 var pi = Math.PI,
     tau = 2*pi;
 
-
+// create a spherical mercator projection
 var projection = d3.geoMercator()
 .scale(1/tau)
 .translate([0,0]); // change to array
@@ -14,8 +16,11 @@ var projection = d3.geoMercator()
 var path = d3.geoPath()
     .projection(projection) //when drawing path using geomercator projection
 
+
+// split window into a quadtree to better render map
 var tile = d3.tile()
 .size([width, height]);
+
 
 var zoom = d3.zoom()
     .scaleExtent([
@@ -23,7 +28,7 @@ var zoom = d3.zoom()
         1 << 24    // max zoom level 
     ]).on('zoom', zoomed);
 
-
+// radius for circle
 var radius = d3.scaleSqrt().range([0,10]);
 
 var svg = d3.select('body')
@@ -32,13 +37,16 @@ var svg = d3.select('body')
 .attr('height', height);
 
 
+// define svg group for raster
 var raster = svg.append('g');
 
 // render to a single path:
 // var vector = svg.append('path');
+
 // render to multiple paths:
 var vector = svg.selectAll('path'); // drawing earthquake location to path
 
+// get the california earthquake data
 d3.json('data/earthquakes_4326_cali.geojson', function(error, geojson) {
   if (error) throw error;
   
@@ -52,11 +60,12 @@ d3.json('data/earthquakes_4326_cali.geojson', function(error, geojson) {
   
   // render to a single path:
   // vector = vector.datum(geojson);
+    
   // render to multiple paths:
   vector = vector
     .data(geojson.features)
     .enter().append('path')
-    .attr('d', path)
+    .attr('d', path) // draw the path
     .on('mouseover', function(d) { console.log(d); });
   
   var center = projection([-119.665, 37.414]);
@@ -72,40 +81,49 @@ d3.json('data/earthquakes_4326_cali.geojson', function(error, geojson) {
 });
 
 
-function zoomed(){
-      var transform = d3.event.transform;
-      var tiles = tile.scale(transform.k)
-                    .translate([transform.x, transform.y])();
-        console.log(transform.x, transform.y, transform.k);
+function zoomed() {
+  var transform = d3.event.transform; 
+
+  var tiles = tile
+    .scale(transform.k)
+    .translate([transform.x, transform.y]) //based on where the user is 
+    ();
+
+  console.log(transform.x, transform.y, transform.k);
+
+  // scale and translate projection for zooming
+  projection
+    .scale(transform.k / tau)
+    .translate([transform.x, transform.y]);
+
+  vector.attr('d', path);
+
     
-    projection.scale(transform.k / tau)
-        .translate([transform.x, transform.y]);
-    
-    vector.attr('d', path);
-    
-    var image = raster
+  // translate the raster grouping and begin a general update pattern w images
+  var image = raster
     .attr('transform', stringify(tiles.scale, tiles.translate))
     .selectAll('image')
-    .data(tiles, function(d){ return d;});
-    
-    image.exit().remove();
-    
-    image.enter().append('image')
+    .data(tiles, function(d) { return d; });
+
+  // remove images which do not bound to data
+  image.exit().remove();
+
+  // for data that do not bound to image
+  image.enter().append('image')
     .attr('xlink:href', function(d) {
-      return 'http://' + 'abc'[d[1] % 3] + '.basemaps.cartocdn.com/rastertiles/voyager/' +
-        d[2] + "/" + d[0] + "/" + d[1] + ".png";
-    })
+      return 'https://' + 'abc'[d[1] % 3] + '.basemaps.cartocdn.com/rastertiles/voyager/' +
+        d[2] + '/' + d[0] + '/' + d[1] + '.png';
+    }) // link to an image based on x, y, zoom factor
     .attr('x', function(d) { return d[0] * 256; })
     .attr('y', function(d) { return d[1] * 256; })
     .attr('width', 256)
     .attr('height', 256);
-}
 
+} 
+
+// create translate string by scaling translate
 function stringify(scale, translate) {
-  var k = scale / 256,
-      r = scale % 1 ? Number : Math.round;
-  return "translate(" + r(translate[0] * scale) + "," + r(translate[1] * scale) + ") scale(" + k + ")";
-}
-
-    
+  var k = scale / 256,  
+      r = scale % 1 ? Number : Math.round;  
+  return 'translate(' + r(translate[0] * scale) + ',' + r(translate[1] * scale) + ') scale (' + k + ')';
 }
